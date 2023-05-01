16d45d7a6b9ec770d4f42faf08f67c4cfc2781ae
faithmwamkinga <faithmwamkinga@gmail.com>
2023-05-01 08:26:50 +0300

    JS-assessment2

---
+++ assessment.js
@@ -0,0 +1,50 @@
+// Create a function called getAvailableBooks that returns an array of all available
+//books.
+function getAvailableBooks(){
+    let foundBooks =[]
+    foundBooks.push(books)
+    
+}
+getAvailableBooks();
+
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+//input author -string and find the author and return 
+//output -array of books
+
+ function getBooksByAuthor(author) {
+    books.find(title)
+   let authorArray=[]
+
+
+   console.log(authorArray);
+    
+ }
+ getBooksByAuthor(author)
+
+//  Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+
+function addNewBook(book) {
+    let book={
+        title:"No longer at Ease",
+        author:"Chinua Achebe",
+        publicationYear: 1960,
+        isAvailable:true
+    }
+    books.add(book)
+    
+}
+
+const books = [
+{ title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+isAvailable: true },
+{ title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+isAvailable: false },
+{ title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+1866, isAvailable: true },
+{ title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+isAvailable: false },
+{ title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+];
